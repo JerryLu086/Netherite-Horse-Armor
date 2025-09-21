6e060bcaf0dd120af2d56916903f5558b22baaed
JerryLu086 <twht.xd@gmail.com>
2025-09-22 01:58:13 +0800

    Datagen w/ item model(s)

--- src/datagen/java/com/jerrylu086/netherite_horse_armor/ModDatagen.java
+++ src/datagen/java/com/jerrylu086/netherite_horse_armor/ModDatagen.java
@@ -6,12 +6,16 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.data.models.BlockModelGenerators;
+import net.minecraft.data.models.ItemModelGenerators;
+import net.minecraft.data.models.model.ModelTemplates;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.network.chat.Component;
@@ -29,6 +33,8 @@ public class ModDatagen implements DataGeneratorEntrypoint {
 
         pack.addProvider(ModRecipeProvider::new);
         pack.addProvider(ModAdvancementProvider::new);
+
+        pack.addProvider(ModItemModelProvider::new);
     }
 
     static class ModRecipeProvider extends FabricRecipeProvider {
@@ -67,6 +73,22 @@ public class ModDatagen implements DataGeneratorEntrypoint {
 
         static AdvancementHolder getAdvancement(String path) {
             return Advancement.Builder.advancement().build(ResourceLocation.parse(path));
+        }
+    }
+
+    static class ModItemModelProvider extends FabricModelProvider {
+        public ModItemModelProvider(FabricDataOutput output) {
+            super(output);
+        }
+
+        @Override
+        public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
+
+        }
+
+        @Override
+        public void generateItemModels(ItemModelGenerators itemModelGenerator) {
+            itemModelGenerator.generateFlatItem(NetheriteHorseArmor.NETHERITE_HORSE_ARMOR, ModelTemplates.FLAT_ITEM);
         }
     }
 }
